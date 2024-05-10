a180c85232e9d87c2d40ef831286994815ec5c4e
Felipe Nogueira <felipe.leite23@gmail.com>
2024-05-10 20:10:40 -0300

    bfs implementado

--- include/algoritmos.py
+++ include/algoritmos.py
@@ -1,12 +1,34 @@
-def bfs():
-    
+from estado import State
+from queue import Queue
+from queue import LifoQueue
+from queue import PriorityQueue
+
+def bfs(currentState: State, n: int):
+    # Fila de prioridade
+    if currentState.currentState == currentState.goalState:
+        return currentState
+
+    fronteira = Queue()
+    fronteira.put(currentState)
+    explorado = set()
+
+    while not fronteira.empty():
+        estado = fronteira.get()
+        explorado.add(tuple(estado.currentState))
+
+        noFilho = estado.expand(n)
+        for filho in noFilho:
+            if tuple(filho.currentState) not in explorado:
+                if filho.checkGoal():
+                    return filho.solution(), len(explorado)
+                fronteira.put(filho)
     return
 
 def dfs():
 
     return
 
-def a_star():
+def aStar():
 
     return
 
