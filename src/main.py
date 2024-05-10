877c4c417d383e3fe4e4154c8f6bccb412867c20
Felipe Nogueira <felipe.leite23@gmail.com>
2024-05-10 20:12:09 -0300

    main e add readme

--- src/main.py
+++ src/main.py
@@ -1,13 +1,21 @@
-from include.estadoInicial import gerar_estado_inicial, solucionavel, print_puzzle
+from include.estadoInicial import gerarEstadoInicial, solucionavel, printPuzzle
+from include.estado import State
+from include.algoritmos import bfs, dfs, aStar
+from time import time
 
 # Exemplo solucionavel [5, 6, 4, 3, 2, 7, 8, 1, 0]
+n = int(input("Insira o tamanho N do puzzle\n")) # Altere o valor para tabuleiro maior
+while (1):
+    root, goal = gerarEstadoInicial(n)
+    if (solucionavel(root)):
+        break
 
-n = int(input("Insira o tamanho N do puzzle\n"))
-while(1):
-  root = gerar_estado_inicial(n) #Altere o valor para tabuleiro maior
-  if(solucionavel(root)):
-     break
-  
 print("Estado inicial:")
-print_puzzle(root)
+printPuzzle(root)
+
+timeBfs = time()
+resultadoBfs = bfs(State(root, None, None, 0, 0, goal), n)
+timeBfs = time() - timeBfs
+print("Tempo de execução BFS: ", timeBfs)
+print("Resultado BFS:", resultadoBfs)
 
